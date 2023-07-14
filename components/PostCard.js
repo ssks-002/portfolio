72913a83f31d380ebabc372c75e196093c57dbd7
ssks-002 <kabi0220@gmail.com>
2023-07-15 03:18:38 +0900

    firstcommit

---
+++ components/PostCard.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Image from "next/image"
+
+/* PostCard component */
+const PostCard = ({ PostData }) =>{
+    const [isOpen, setIsOpen] = useState(false)
+    const Mordal = () => {
+        setIsOpen((prevState) => !prevState)
+    };
+
+    return(
+        <>
+        {!PostData.visible && 
+        <>
+        <div className={utilStyles.thumbnail_container} onClick={Mordal}>
+            {isOpen && 
+                <>
+                    <label className={utilStyles.opendfilter} />
+                    <div className={utilStyles.workdetailarea}>
+                        <div className={utilStyles.workdetailarea_container}>
+                            <div className={utilStyles.workdetail_container}>
+                                <div className={utilStyles.workdetail}>
+                                    <h2 className={utilStyles.worktitle}>{PostData.title}</h2>
+                                    {PostData.description &&
+                                    <p className={utilStyles.workdescription}>{PostData.description}</p>
+                                    }
+                                    {PostData.downdloadlink &&
+                                    <div className={utilStyles.workdownload}>
+                                        Projectfile
+                                    <Link href={PostData.downdloadlink}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                                        </svg>
+                                    </Link>
+                                    </div>
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </> 
+            }
+            <div className={utilStyles.overlay}>
+                <h2 className={utilStyles.title}>{PostData.title}</h2>
+            </div>
+            <Image className={utilStyles.thumbnail} src={PostData.maincontent.url}  fill/>
+            
+        </div>
+        </> 
+        }
+        </>
+    );
+};
+
+export default PostCard;
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,67 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
+import Script from 'next/script'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import PostCard from '../components/PostCard';
+
+const siteTitle = 'portfolio'
 
 export default function Home({ allPostsData }) {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
+    <div className={utilStyles.container}>
+    <Head>
+      <title>portfolio site</title>
+      <meta
+        name="description"
+        content="portfolio site using Next.js"
+      />
+      <meta
+        property="og:image"
+        content={`https://og-image.vercel.app/${encodeURI(
+          siteTitle
+        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+      />
+      <meta name="og:title" content={siteTitle} />
+      <meta name="twitter:card" content="summary_large_image" />
+    </Head>
+    <Script
+      src="https://connect.facebook.net/en_US/sdk.js"
+      strategy="lazyOnload"
+      onLoad={() =>
+        console.log(`script loaded correctly, window.FB has been populated`)
+      }
+    />
+  
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Works</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map((PostData) => (
+          <PostCard key={PostData.id} PostData={PostData} />
+          ))}
+          {/*{allPostsData.map(({ id, createdAt, title, maincontent }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
+              <div className={utilStyles.thumbnail_container}>
+              <Image className={utilStyles.thumbnail} src={maincontent.url}  fill/>
+              </div>
               <br />
               <small className={utilStyles.lightText}>
-                <Date dateString={date} />
+                <Date dateString={createdAt} />
               </small>
-            </li>
-          ))}
+          </li>
+          ))}*/}
         </ul>
       </section>
-    </Layout>
+    </div>
   )
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = await getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
-}
+      allPostsData,
+    },
+  };
+};
